68a871d82c9e6ac96f6b815a65f8c025e2664750
Lars Tinnefeld <50445480+LarsTinnefeld@users.noreply.github.com>
2021-03-12 08:34:47 -0500

    Started new notebook
    New notebook for RFM

---
+++ 3 - Customer Segmentation/Olist_classes.py
@@ -0,0 +1,15 @@
+class Olist_analysis():
+
+    def __init__(self):
+
+        print('Class initialized')
+
+    def test_calcs(self, x):
+        
+        self.x = x
+        self.y = self.x + 10
+        return self.y
+
+    def greet(self):
+        print('Howdy!')
+
