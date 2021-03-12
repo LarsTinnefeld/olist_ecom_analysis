8941f9693cbafe0b5847d4f0f3b73846951226bc
Lars Tinnefeld <50445480+LarsTinnefeld@users.noreply.github.com>
2021-03-12 08:37:32 -0500

    Updates
    Updates

--- 3 - Customer Segmentation/Olist_classes.py
+++ 3 - Customer Segmentation/Olist_classes.py
@@ -2,12 +2,11 @@ class Olist_analysis():
 
     def __init__(self):
 
-        print('Class initialized')
+        print('Olist analysis class initialized')
 
     def test_calcs(self, x):
         
-        self.x = x
-        self.y = self.x + 10
+        self.y = x + 10
         return self.y
 
     def greet(self):
